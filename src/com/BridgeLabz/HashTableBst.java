217147bc5b636fec1407dbaac5a85ca3707d44a5
PatilBhavesh84 <bhaveshpatil8448@gmail.com>
2022-07-31 10:55:49 +0530

    Added:Frequency check

--- src/com/BridgeLabz/HashTableBst.java
+++ src/com/BridgeLabz/HashTableBst.java
@@ -1,5 +1,29 @@
 package com.BridgeLabz;
-
+import java.util.HashMap;
+import java.util.Set;
 public class HashTableBst {
+	static void sentence(String inputString){
 
+		HashMap<String, Integer> count = new HashMap<String,Integer>();
+		String[] words = inputString.split(" ");
+
+		for(String word : words){
+			if(count.containsKey(word)){
+				count.put(word, count.get(word)+1);             
+			}
+			else{
+				count.put(word, 1);
+			}
+		}
+		Set<String> wordsInString = count.keySet();
+		System.out.println("Frequency of words in sentence 'To be or not to be' is");
+		for(String word : wordsInString){
+			if(count.get(word)>1){				
+				System.out.println(word+" : "+count.get(word));
+			}
+		}
+	}
+	public static void main(String args[]){
+		sentence("To be or not To be");
+	}
 }
