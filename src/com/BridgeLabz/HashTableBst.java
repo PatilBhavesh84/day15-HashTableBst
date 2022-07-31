ed6d2db61d4198787967a8f67736d5f2876c1057
PatilBhavesh84 <bhaveshpatil8448@gmail.com>
2022-07-31 11:08:27 +0530

    Added:Remove word 'avoidable'

--- src/com/BridgeLabz/HashTableBst.java
+++ src/com/BridgeLabz/HashTableBst.java
@@ -1,32 +1,26 @@
 package com.BridgeLabz;
-import java.util.HashMap;
-import java.util.Set;
 public class HashTableBst {
-	static void sentence(String inputString){
 
-		HashMap<String, Integer> count = new HashMap<String,Integer>();
-		String[] words = inputString.split(" ");
+	static void remove(String str, String word){
 
-		for(String word : words){
-			if(count.containsKey(word)){
-				count.put(word, count.get(word)+1);             
-			}
-			else{
-				count.put(word, 1);
+		String msg[] = str.split(" ");
+		String new_str = "";
+
+		for (String words : msg) {
+			if (!words.equals(word)) {
+				new_str += words + " ";
 			}
 		}
-		Set<String> wordsInString = count.keySet();
-		System.out.println("Frequency of words in sentence 'Paranoids are not paranoid because"
-				+ " they are paranoid but because they keep putting themselves deliberately into"
-				+ " paranoid avoidable situations' is");
-		for(String word : wordsInString){
-			if(count.get(word)>1){				
-				System.out.println(word+" : "+count.get(word));
-			}
-		}
+		System.out.print(new_str);
 	}
-	public static void main(String args[]){
-		sentence("Paranoids are not paranoid because they are paranoid but because "
-				+ "they keep putting themselves deliberately into paranoid avoidable situations");
+	public static void main(String[] args)
+	{
+		String str = "Paranoids are not paranoid because"
+				+ " they are paranoid but because they keep"
+				+ " putting themselves deliberately into"
+				+ " paranoid avoidable situations";
+
+		String word = "avoidable";
+		remove(str, word);
 	}
 }
